08179dc2b7539ded78333ba8c4059fc47a0eda1b
vianagabrielr8 <gvianacomercial@gmail.com>
2025-12-09 11:13:30 -0300

    Add login route to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Login from "./pages/Login";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -35,6 +36,7 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
+            <Route path="/login" element={<Login />} />
             <Route path="/" element={<Index />} />
             <Route path="/estoque" element={<Estoque />} />
             <Route path="/compras" element={<Compras />} />
