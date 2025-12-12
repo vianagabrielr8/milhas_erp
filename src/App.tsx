a447de3ca5c5131d35bfc17c34a9ade8e88af588
vianagabrielr8 <gvianacomercial@gmail.com>
2025-12-12 17:29:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,22 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// PÁGINAS CONFIRMADAS NO SEU PRINT
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Estoque from "./pages/Estoque";
-import Compras from "./pages/Compras";
-import Vendas from "./pages/Vendas";
 import Transferencias from "./pages/Transferencias";
+import Vendas from "./pages/Vendas";
 import ContasPagar from "./pages/ContasPagar";
 import ContasReceber from "./pages/ContasReceber";
-import CartoesPagamento from "./pages/CartoesPagamento";
-import Contas from "./pages/Contas";
 import Programas from "./pages/Programas";
-import Clientes from "./pages/Clientes";
 import Fornecedores from "./pages/Fornecedores";
 import Limites from "./pages/Limites";
+import Financeiro from "./pages/Financeiro"; 
+// import Compras from "./pages/Compras"; // Descomente se existir
+// import Clientes from "./pages/Clientes"; // Descomente se existir
 
 const queryClient = new QueryClient();
 
@@ -26,21 +27,29 @@ const App = () => (
       <Toaster />
       <BrowserRouter>
         <Routes>
+          {/* Rotas Principais */}
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          
+          {/* Operacional */}
           <Route path="/estoque" element={<Estoque />} />
-          <Route path="/compras" element={<Compras />} />
           <Route path="/vendas" element={<Vendas />} />
           <Route path="/transferencias" element={<Transferencias />} />
+          
+          {/* Financeiro */}
+          <Route path="/financeiro" element={<Financeiro />} />
           <Route path="/financeiro/pagar" element={<ContasPagar />} />
           <Route path="/financeiro/receber" element={<ContasReceber />} />
-          <Route path="/financeiro/cartoes" element={<CartoesPagamento />} />
-          <Route path="/cadastros/contas" element={<Contas />} />
+          
+          {/* Cadastros e Configurações */}
           <Route path="/cadastros/programas" element={<Programas />} />
-          <Route path="/cadastros/clientes" element={<Clientes />} />
           <Route path="/cadastros/fornecedores" element={<Fornecedores />} />
           <Route path="/seguranca/limites" element={<Limites />} />
+          
+          {/* Rotas Comentadas (Para evitar erro se o arquivo não existir) */}
+          {/* <Route path="/compras" element={<Compras />} /> */}
+          {/* <Route path="/cadastros/clientes" element={<Clientes />} /> */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
